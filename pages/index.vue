1bae6ed0c3b0e2421249450cbab05979452f0a84
yoonpoon <yoonpoon@gmail.com>
2018-11-13 22:55:26 -0800

    nice

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container">
     <div class="row">
-      <div class="col s12 col m6 col l4">
+      <div class="col s12 col m6 col l4 sr-container">
         <a 
           class="sr-link" 
           href="#"> 
@@ -10,7 +10,7 @@
         <h2 class="sr-url">https://en.wikipedia.org/wiki/Vanessa_Place</h2>
         <p> May 1, 2018 - “Ergo, echo,” Vanessa Place has said of herself.[4] But if, on the one hand, Echo adds to our artifacts, augmenting our understanding — Echo ... </p>
       </div>
-      <div class="col s12 col m6 col l4">
+      <div class="col s12 col m6 col l4 sr-container">
         <a 
           class="sr-link" 
           href="#"> 
@@ -19,7 +19,7 @@
         <h2 class="sr-url">https://en.wikipedia.org/wiki/Vanessa_Place</h2>
         <p> May 1, 2018 - “Ergo, echo,” Vanessa Place has said of herself.[4] But if, on the one hand, Echo adds to our artifacts, augmenting our understanding — Echo ... </p>
       </div>
-      <div class="col s12 col m6 col l4">
+      <div class="col s12 col m6 col l4 sr-container">
         <a 
           class="sr-link" 
           href="#"> 
@@ -27,7 +27,34 @@
         </a>
         <h2 class="sr-url">https://en.wikipedia.org/wiki/Vanessa_Place</h2>
         <p> May 1, 2018 - “Ergo, echo,” Vanessa Place has said of herself.[4] But if, on the one hand, Echo adds to our artifacts, augmenting our understanding — Echo ... </p>
-      </div>      
+      </div> 
+      <div class="col s12 col m6 col l4 sr-container">
+        <a 
+          class="sr-link" 
+          href="#"> 
+          <h1 class="sr-link-title"> Vanessa Place - Poet | Academy of American Poets </h1> 
+        </a>
+        <h2 class="sr-url">https://en.wikipedia.org/wiki/Vanessa_Place</h2>
+        <p> May 1, 2018 - “Ergo, echo,” Vanessa Place has said of herself.[4] But if, on the one hand, Echo adds to our artifacts, augmenting our understanding — Echo ... </p>
+      </div>
+      <div class="col s12 col m6 col l4 sr-container">
+        <a 
+          class="sr-link" 
+          href="#"> 
+          <h1 class="sr-link-title"> Vanessa Place - Poet | Academy of American Poets </h1> 
+        </a>
+        <h2 class="sr-url">https://en.wikipedia.org/wiki/Vanessa_Place</h2>
+        <p> May 1, 2018 - “Ergo, echo,” Vanessa Place has said of herself.[4] But if, on the one hand, Echo adds to our artifacts, augmenting our understanding — Echo ... </p>
+      </div>
+      <div class="col s12 col m6 col l4 sr-container">
+        <a 
+          class="sr-link" 
+          href="#"> 
+          <h1 class="sr-link-title"> Vanessa Place - Poet | Academy of American Poets </h1> 
+        </a>
+        <h2 class="sr-url">https://en.wikipedia.org/wiki/Vanessa_Place</h2>
+        <p> May 1, 2018 - “Ergo, echo,” Vanessa Place has said of herself.[4] But if, on the one hand, Echo adds to our artifacts, augmenting our understanding — Echo ... </p>
+      </div>                       
     </div>
   </section>
 </template>
@@ -44,16 +71,15 @@ export default {
 
 <style lang="scss" scoped>
 html {
-  line-height: 1.2;
-  font-family: 'arial';
   h1 {
     font-size: 18px;
+    margin-bottom: 5px;
+    font-weight: lighter;
   }
   h2 {
     font-size: 14px;
-  }
-  h3 {
-    font-size: 13px;
+    margin-bottom: 5px;
+    font-weight: lighter;
   }
   a {
     color: #1a0dab;
@@ -61,8 +87,8 @@ html {
 }
 .container {
   .row {
-    .sr-link-title {
-      margin-bottom: 5px;
+    .sr-container {
+      margin-bottom: 20px;
     }
     .sr-url {
       color: #006621;
